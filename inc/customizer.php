3c93ead9471845cd94385a153d1dd9f5bee33f17
brmayes <brmayes@live.unc.edu>
2016-07-11 22:10:32 -0400

    first commit or new starter template.

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * _portfolio Theme Customizer.
+ *
+ * @package _portfolio
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function _portfolio_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', '_portfolio_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function _portfolio_customize_preview_js() {
+	wp_enqueue_script( '_portfolio_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', '_portfolio_customize_preview_js' );
