46d4d0076aefde8d8fae27fb4652cfd6c2ce9c9b
brmayes <brmayes@live.unc.edu>
2016-08-05 09:30:15 -0400

    finished the homepage template. Really excited about this project and how much progress I make when I actively try to commit at least once a day

---
+++ page-work.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying thoughts page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _portfolio
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div id="page-work">
+				<?php
+					$slug = basename(get_permalink());
+					// echo '<h1 class="title">' . $slug . '</h1>';
+
+					$args = array(
+						'posts_per_page' =>100,
+						'cat' => 2,
+						'order' => 'DESC',
+						'orderby' => 'date'
+					);
+					$loop = new WP_Query( $args );
+
+					if( $loop->have_posts() ) :
+						single_cat_title();
+
+					while( $loop->have_posts() ) : $loop->the_post();
+						get_template_part( 'template-parts/content-work' );
+
+					endwhile;
+
+					endif;
+					wp_reset_postdata();
+				?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// get_sidebar();
+get_footer();
