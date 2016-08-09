72fac0c7e1b15aa2ab495ad8147dd22c4c26f529
brmayes <brmayes@live.unc.edu>
2016-08-08 20:46:40 -0400

    Trying to iron out the fade on the work page. Not really working.

--- page-work.php
+++ page-work.php
@@ -13,97 +13,102 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div id="page-work">
 				<ul>
-					<li class="trigger"><a href="#box1">All</a></li>
-					<li class="trigger"><a href="#box2">Graphic Design</a></li>
-					<li class="trigger"><a href="#box3">Interactive Media</a></li>
+					<li class="trigger"><a class="tag-link" href="#box1">All</a></li>
+					<li class="trigger"><a class="tag-link" href="#box2">Graphic Design</a></li>
+					<li class="trigger"><a class="tag-link" href="#box3">Interactive Media</a></li>
 				</ul>
-				<div class ="toggle" id="box1">
-					<div class="posts-container">
-						<?php
-							$args = array(
-								'posts_per_page' =>100,
-								'cat' => 2,
-								'order' => 'DESC',
-								'orderby' => 'date'
-							);
-							$loop = new WP_Query( $args );
+				<div class="portfolio-space">
+					<div class ="toggle" id="box1">
+						<div class="posts-container">
+							<?php
+								$args = array(
+									'posts_per_page' =>100,
+									'cat' => 2,
+									'order' => 'DESC',
+									'orderby' => 'date'
+								);
+								$loop = new WP_Query( $args );
 
-							if( $loop->have_posts() ) :
-								single_cat_title();
+								if( $loop->have_posts() ) :
+									single_cat_title();
 
-							while( $loop->have_posts() ) : $loop->the_post();
-								get_template_part( 'template-parts/content-work' );
+								while( $loop->have_posts() ) : $loop->the_post();
+									get_template_part( 'template-parts/content-work' );
 
-							endwhile;
+								endwhile;
 
-							endif;
-							wp_reset_postdata();
-						?>
+								endif;
+								wp_reset_postdata();
+							?>
+						</div>
 					</div>
-				</div>
 
-				<div class ="toggle" id="box2">
-					<div class="posts-container">
-						<?php
-							$tag = 'graphic-design';
-							$args = array(
-								'posts_per_page' =>100,
-								'cat' => 2,
-								'order' => 'DESC',
-								'orderby' => 'date',
-								'tag' => 'graphic-design'
-							);
-							$loop = new WP_Query( $args );
+					<div class ="toggle" id="box2">
+						<div class="posts-container">
+							<?php
+								$tag = 'graphic-design';
+								$args = array(
+									'posts_per_page' =>100,
+									'cat' => 2,
+									'order' => 'DESC',
+									'orderby' => 'date',
+									'tag' => 'graphic-design'
+								);
+								$loop = new WP_Query( $args );
 
-							if( $loop->have_posts() ) :
-								single_cat_title();
+								if( $loop->have_posts() ) :
+									single_cat_title();
 
-							while( $loop->have_posts() ) : $loop->the_post();
-								get_template_part( 'template-parts/content-work' );
+								while( $loop->have_posts() ) : $loop->the_post();
+									get_template_part( 'template-parts/content-work' );
 
-							endwhile;
+								endwhile;
 
-							endif;
-							wp_reset_postdata();
-						?>
+								endif;
+								wp_reset_postdata();
+							?>
+						</div>
 					</div>
-				</div>
 
-				<div class ="toggle" id="box3">
-					<div class="posts-container">
-						<?php
-							$tag = 'interactive-media';
-							$args = array(
-								'posts_per_page' =>100,
-								'cat' => 2,
-								'order' => 'DESC',
-								'orderby' => 'date',
-								'tag' => $tag
-							);
-							$loop = new WP_Query( $args );
+					<div class ="toggle" id="box3">
+						<div class="posts-container">
+							<?php
+								$tag = 'interactive-media';
+								$args = array(
+									'posts_per_page' =>100,
+									'cat' => 2,
+									'order' => 'DESC',
+									'orderby' => 'date',
+									'tag' => $tag
+								);
+								$loop = new WP_Query( $args );
 
-							if( $loop->have_posts() ) :
-								single_cat_title();
+								if( $loop->have_posts() ) :
+									single_cat_title();
 
-							while( $loop->have_posts() ) : $loop->the_post();
-								get_template_part( 'template-parts/content-work' );
+								while( $loop->have_posts() ) : $loop->the_post();
+									get_template_part( 'template-parts/content-work' );
 
-							endwhile;
+								endwhile;
 
-							endif;
-							wp_reset_postdata();
-						?>
+								endif;
+								wp_reset_postdata();
+							?>
+						</div>
 					</div>
 				</div>
 
 				<script type="text/javascript">
 					jQuery(function($){
-						$("a").click(function($e){
+						$("a.tag-link").click(function($e){
+							$(".portfolio-space").fadeOut('fast');
 							$e.preventDefault();
-						   var myelement = $(this).attr("href")
-							 	$(myelement).fadeOut( 1200 );
-						    $(myelement).fadeIn( 1200 );
-						    $(".toggle:visible").not(myelement).hide();
+
+							var myelement = $(this).attr("href");
+							console.log(myelement);
+							$(myelement).show();
+							$(".toggle:visible").not(myelement).hide();
+							$(".portfolio-space").delay().fadeIn();
 						});
 					});
 				</script>
